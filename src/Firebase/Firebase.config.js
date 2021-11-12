7e23ff07990dd2c67a803fec5ea135583c774b7f
Emon Ahmed <technicalboyemon@gmail.com>
2021-11-12 22:46:16 +0600

    Update

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,17 +1,10 @@
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_APIKEY,
-  // authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  // projectId: process.env.REACT_APP_PROJECTID,
-  // storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  // messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  // appId: process.env.REACT_APP_APPID,
-  
-  apiKey: "AIzaSyAWkCTimrrMvOC8Agu-kmfJdhu41nEN2Sk",
-  authDomain: "woodcraft-shop.firebaseapp.com",
-  projectId: "woodcraft-shop",
-  storageBucket: "woodcraft-shop.appspot.com",
-  messagingSenderId: "483550905988",
-  appId: "1:483550905988:web:6f1e82f9b3fac89f55e4d1",
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
 };
 
 export default firebaseConfig;
